3234b7d1007980ceda099cfbc9c0211934fa0bd9
moglione1 <86819387+moglione1@users.noreply.github.com>
2021-07-26 06:54:39 +0100

    Add files via upload

---
+++ rocketlaunch.py
@@ -0,0 +1,7 @@
+dcc.Dropdown(id='site-dropdown', 
+                   options=[
+                           {'label': 'CCAFS LC-40', 'value': 'CCAFS LC-40'},
+                           {'label': 'NAFB 8LC-4E', 'value': 'NAFB 8LC-4E'}
+                           ],
+                  placeholder='Select a Launch Site',
+                  style={....})
